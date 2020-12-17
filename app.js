8fce0771b1a7822f41c474a3d32fe7f07f5da13a
esuntumur <esontomorgantomor@gmail.com>
2020-12-17 22:32:28 +0800

    hereglegchin oruulsan utgiig huleen avah, css file section

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
+// Дэлгэцтэй ажиллах контроллер
 var uiController = (function () {
-  var x = 100;
-
-  function add(y) {
-    return x + y;
-  }
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
   return {
-    publicAdd: function (a) {
-      a = add(a);
-      console.log("a :>> ", a);
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
     },
   };
 })();
 
+// Санхүүтэй ажиллах контроллер
 var financeController = (function () {})();
 
-var appController = (function () {
+// Програмын холбогч контроллер
+var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDOMstrings();
+
   var ctrlAddItem = function () {
     // 1. оруулах өгөгдлийг дэлгэцнээс олж авна.
-    console.log("дэлгэцээс өгөгдөл авна.");
+
+    console.log(uiController.getInput());
+
     // 2. олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
     // 3. олж авсан өгөгдлүүдээ веб дээрх тохирох хэсгүүдэд гаргана.add
     // 4. төсвийг тооцоолно.
     // 5. эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
     ctrlAddItem();
   });
 
   document.addEventListener("keypress", function (event) {
     event.keyCode === 13 || event.which === 13 ? ctrlAddItem() : 0;
   });
-})(uiController);
+})(uiController, financeController);
